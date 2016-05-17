6959b8ec906c9ae602dd22eb37aa9978a537d2b3
belgica <belgica@berkeley.edu>
2016-05-17 23:47:10 +0000

    Commit on may 17

---
+++ client/main.js
@@ -0,0 +1,58 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+
+Template.upload.onCreated( () => {
+  Template.instance().uploading = new ReactiveVar( false );
+});
+
+Template.upload.helpers({
+  uploading() {
+    return Template.instance().uploading.get();
+  }
+});
+
+Template.upload.events({
+  'change [name="uploadCSV"]' ( event, template ) {
+
+    Papa.parse( event.target.files[0], {
+      header: true,
+      complete( results, file ) {
+        Meteor.call( 'parseUpload', results.data, ( error, response ) => {
+          if ( error ) {
+            console.log( error.reason );
+          } else {
+            template.uploading.set( false );
+            Bert.alert( 'Upload complete!', 'success', 'growl-top-right' );
+          }
+        });
+      }
+    });
+  }
+});
+
+
+/*
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+*/
+
+
