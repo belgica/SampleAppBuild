76e794028df580b3b602ce7e6e90736a45137c1b
belgica <belgica@berkeley.edu>
2016-05-25 03:10:43 +0000

    Added user login

--- client/main.js
+++ client/main.js
@@ -1,9 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
-
-
 Template.upload.onCreated( () => {
   Template.instance().uploading = new ReactiveVar( false );
 });
@@ -69,5 +66,3 @@ Template.hello.events({
   },
 });
 */
-
-
